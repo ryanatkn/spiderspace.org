75937f0474cfe6dab151ed5ad34728eaf4918c18
Ryan Atkinson <mail@ryanatkn.com>
2024-03-24 12:17:22 -0600

    add intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 	const pkg = parse_package_meta(package_json.homepage, package_json, src_json);
 </script>
 
-<main class="prose">
+<main class="prose width_md">
 	<section class="box">
 		<header class="prose box mb_xl4">
 			<h1>8000.spiderspace.org</h1>
@@ -20,6 +20,31 @@
 			<div slot="icon">🕸️</div>
 			about
 		</Card>
+	</section>
+	<section class="panel p_md">
+		<p>
+			It's 2024 when I'm writing this and we as a web are currently searching for sustainable ways
+			to fund free and open source software. I'm a software developer and this is my evolving
+			attempt at funding my projects without any investors, sponsors, or other business partnerships
+			in the mix.
+		</p>
+		<p>
+			I started as a professional web developer focusing on JS in 2011, and 13 years later I'm still
+			enjoying it. My goal is to work for the web on the projects I find meaningful. Currently
+			that's my projects <a href="https://www.fuz.dev/">Fuz</a>,
+			<a href="https://gro.ryanatkn.com/">Gro</a>, the (not yet published) social app framework
+			<a href="https://zzz.fuz.dev/">Zzz</a>, and many of their dependencies. I'm communicating
+			about them on YouTube with
+			<a href="https://youtube.com/@spiderspace_8000">Spacespace</a>
+			and <a href="https://youtube.com/@webdevladder">webdevladder</a>.
+		</p>
+		<p>
+			If you want to see more software and videos and blog posts from me where my incentives are as
+			pure as the + function, this website has links for supporting me. Giving me any amount of
+			money gives you access to supporter-only community spaces. <a
+				href="https://discord.gg/YU5tyeK72X">Join the Discord</a
+			> to get a feel for the vibes.
+		</p>
 	</section>
 	<section>
 		<ul>
