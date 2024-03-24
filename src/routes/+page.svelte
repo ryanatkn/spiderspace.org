bd2199ab0d6cb475ac4e79586c1677609ba5d3af
Ryan Atkinson <mail@ryanatkn.com>
2024-03-24 12:24:31 -0600

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,73 +18,79 @@
 		</header>
 	</section>
 	<section class="panel p_md">
-		<p>
-			It's 2024 when I'm writing this and we as a web are currently searching for sustainable ways
-			to fund free and open source software. I'm a software developer and this is my evolving
-			attempt at funding my projects without any investors, sponsors, or other business partnerships
-			in the mix.
-		</p>
-		<p>
-			I started as a professional web developer focusing on JS in 2011, and 13 years later I'm still
-			enjoying it. My goal is to work for the web on the projects I find meaningful. Currently
-			that's my projects <a href="https://www.fuz.dev/">Fuz</a>,
-			<a href="https://gro.ryanatkn.com/">Gro</a>, the (not yet published) social app framework
-			<a href="https://zzz.fuz.dev/">Zzz</a>, and many of their dependencies. I've been working on
-			them full-time in public since 2019 while living off my savings, and I'm communicating about
-			them on YouTube with
-			<a href="https://youtube.com/@spiderspace_8000">Spacespace</a>
-			and <a href="https://youtube.com/@webdevladder">webdevladder</a>.
-		</p>
-		<p>
-			If you want to see more software and videos and blog posts from me where my incentives are as
-			pure as the + function, this website has links for supporting me. Giving me any amount of
-			money gives you access to supporter-only community spaces. <a
-				href="https://discord.gg/YU5tyeK72X">Join the Discord</a
-			> to get a feel for the vibes.
-		</p>
-	</section>
-	<section>
-		<ul>
-			<li>
-				<a rel="me" href="https://discord.gg/YU5tyeK72X">Discord</a>
-			</li>
-			<li>
-				<a rel="me" href="https://www.spiderspace.org/">spiderspace.org</a>
-			</li>
-			<li>
-				<a rel="me" href="https://www.webdevladder.net/">webdevladder.net</a>
-			</li>
-			<li>
-				<a rel="me" href="https://www.ryanatkn.com/">ryanatkn.com</a>
-			</li>
-			<li>
-				<a rel="me" href="https://youtube.com/@spiderspace_8000">youtube.com/@spiderspace_8000</a>
-			</li>
-			<li>
-				<a rel="me" href="https://youtube.com/@webdevladder">youtube.com/@webdevladder</a>
-			</li>
-			<li>
-				<a rel="me" href="https://github.com/spiderspace">github.com/spiderspace</a>
-			</li>
-			<li>
-				<a rel="me" href="https://github.com/webdevladder">github.com/webdevladder</a>
-			</li>
-			<li>
-				<a rel="me" href="https://mastodon.social/@webdevladder">@webdevladder@mastodon.social</a>
-			</li>
-			<li>
-				<a rel="me" href="https://hci.social/@ryanatkn">@ryanatkn@hci.social</a>
-			</li>
-			<li>
-				<a rel="me" href="https://twitter.com/webdevladder">twitter.com/webdevladder</a>
-			</li>
-			<li>
-				<a rel="me" href="https://github.com/sponsors/ryanatkn">github.com/sponsors/ryanatkn</a>
-			</li>
-			<li>
-				<a rel="me" href="https://www.patreon.com/ryanatkn">patreon.com/ryanatkn</a>
-			</li>
-		</ul>
+		<div class="panel bg p_md">
+			<p>
+				It's 2024 when I'm writing this and we as a web are currently searching for sustainable ways
+				to fund free and open source software. I'm a software developer and this is my evolving
+				attempt at funding my projects without any investors, sponsors, or other business
+				partnerships in the mix.
+			</p>
+			<p>
+				I started as a professional web developer focusing on JS in 2011, and 13 years later I'm
+				still enjoying it. My goal is to work for the web on the projects I find meaningful.
+				Currently that's my projects <a href="https://www.fuz.dev/">Fuz</a>,
+				<a href="https://gro.ryanatkn.com/">Gro</a>, the (not yet published) social app framework
+				<a href="https://zzz.fuz.dev/">Zzz</a>, and many of their dependencies. I've been working on
+				them full-time in public since 2019 while living off my savings, and I'm communicating about
+				them on YouTube with
+				<a href="https://youtube.com/@spiderspace_8000">Spacespace</a>
+				and <a href="https://youtube.com/@webdevladder">webdevladder</a>.
+			</p>
+			<p>
+				If you want to see more software and videos and blog posts from me where my incentives are
+				as pure as the + function, this website has links for supporting me. Giving me any amount of
+				money gives you access to supporter-only community spaces. <a
+					href="https://discord.gg/YU5tyeK72X">Join the Discord</a
+				> to get a feel for the vibes.
+			</p>
+			<aside>
+				<ul>
+					<li>
+						<a rel="me" href="https://discord.gg/YU5tyeK72X">Discord</a>
+					</li>
+					<li>
+						<a rel="me" href="https://www.spiderspace.org/">spiderspace.org</a>
+					</li>
+					<li>
+						<a rel="me" href="https://www.webdevladder.net/">webdevladder.net</a>
+					</li>
+					<li>
+						<a rel="me" href="https://www.ryanatkn.com/">ryanatkn.com</a>
+					</li>
+					<li>
+						<a rel="me" href="https://youtube.com/@spiderspace_8000"
+							>youtube.com/@spiderspace_8000</a
+						>
+					</li>
+					<li>
+						<a rel="me" href="https://youtube.com/@webdevladder">youtube.com/@webdevladder</a>
+					</li>
+					<li>
+						<a rel="me" href="https://github.com/spiderspace">github.com/spiderspace</a>
+					</li>
+					<li>
+						<a rel="me" href="https://github.com/webdevladder">github.com/webdevladder</a>
+					</li>
+					<li>
+						<a rel="me" href="https://mastodon.social/@webdevladder"
+							>@webdevladder@mastodon.social</a
+						>
+					</li>
+					<li>
+						<a rel="me" href="https://hci.social/@ryanatkn">@ryanatkn@hci.social</a>
+					</li>
+					<li>
+						<a rel="me" href="https://twitter.com/webdevladder">twitter.com/webdevladder</a>
+					</li>
+					<li>
+						<a rel="me" href="https://github.com/sponsors/ryanatkn">github.com/sponsors/ryanatkn</a>
+					</li>
+					<li>
+						<a rel="me" href="https://www.patreon.com/ryanatkn">patreon.com/ryanatkn</a>
+					</li>
+				</ul>
+			</aside>
+		</div>
 	</section>
 	<section>
 		<Card href="{base}/about">
