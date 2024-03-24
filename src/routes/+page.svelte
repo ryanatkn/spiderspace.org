bad911dd63c83233950680e00f1972cda5ffe3fc
Ryan Atkinson <mail@ryanatkn.com>
2024-03-23 19:13:23 -0600

    add links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import Card from '@ryanatkn/fuz/Card.svelte';
 	import {base} from '$app/paths';
+	import {parse_package_meta} from '@ryanatkn/gro/package_meta.js';
+	import Library_Footer from '@ryanatkn/fuz/Library_Footer.svelte';
+
+	import {package_json, src_json} from '$routes/package.js';
+
+	// TODO put in context?
+	const pkg = parse_package_meta(package_json.homepage, package_json, src_json);
 </script>
 
 <main class="prose">
@@ -13,6 +20,52 @@
 			<div slot="icon">🕸️</div>
 			about
 		</Card>
+	</section>
+	<section>
+		<ul>
+			<li>
+				<a rel="me" href="https://discord.gg/YU5tyeK72X">Discord</a>
+			</li>
+			<li>
+				<a rel="me" href="https://www.spiderspace.org/">spiderspace.org</a>
+			</li>
+			<li>
+				<a rel="me" href="https://www.webdevladder.net/">webdevladder.net</a>
+			</li>
+			<li>
+				<a rel="me" href="https://www.ryanatkn.com/">ryanatkn.com</a>
+			</li>
+			<li>
+				<a rel="me" href="https://youtube.com/@spiderspace_8000">youtube.com/@spiderspace_8000</a>
+			</li>
+			<li>
+				<a rel="me" href="https://youtube.com/@webdevladder">youtube.com/@webdevladder</a>
+			</li>
+			<li>
+				<a rel="me" href="https://github.com/spiderspace">github.com/spiderspace</a>
+			</li>
+			<li>
+				<a rel="me" href="https://github.com/webdevladder">github.com/webdevladder</a>
+			</li>
+			<li>
+				<a rel="me" href="https://mastodon.social/@webdevladder">@webdevladder@mastodon.social</a>
+			</li>
+			<li>
+				<a rel="me" href="https://hci.social/@ryanatkn">@ryanatkn@hci.social</a>
+			</li>
+			<li>
+				<a rel="me" href="https://twitter.com/webdevladder">twitter.com/webdevladder</a>
+			</li>
+			<li>
+				<a rel="me" href="https://github.com/sponsors/ryanatkn">github.com/sponsors/ryanatkn</a>
+			</li>
+			<li>
+				<a rel="me" href="https://www.patreon.com/ryanatkn">patreon.com/ryanatkn</a>
+			</li>
+		</ul>
+	</section>
+	<section>
+		<Library_Footer {pkg} />
 	</section>
 </main>
 
