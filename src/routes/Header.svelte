b709fe6b1337df61eee20e6a46212a841437bcfc
Ryan Atkinson <mail@ryanatkn.com>
2024-09-23 10:07:02 -0600

    update logos

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import Spiderspace_Logo from '@ryanatkn/fuz/Spiderspace_Logo.svelte';
+	import Svg from '@ryanatkn/fuz/Svg.svelte';
+	import {spiderspace_logo} from '@ryanatkn/fuz/logos.js';
 </script>
 
 <header class="box">
 	<h1 class="my_xl5">spiderspace</h1>
-	<Spiderspace_Logo size="var(--icon_size_xl3)" />
+	<Svg data={spiderspace_logo} size="var(--icon_size_xl3)" />
 	<div class="mt_xl4 shadow_inset_bottom_xs p_sm radius_md">
 		<blockquote class="shadow_inset_xs px_xl">weaving web community 🕸️</blockquote>
 	</div>
