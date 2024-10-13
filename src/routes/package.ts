f61365f329d590ed5fad6caab700433bbe9fa130
Ryan Atkinson <mail@ryanatkn.com>
2024-10-13 05:23:42 -0600

    upgrade fuz

--- src/routes/package.ts
+++ src/routes/package.ts
@@ -32,8 +32,8 @@ export const package_json = {
 	devDependencies: {
 		'@ryanatkn/belt': '^0.25.3',
 		'@ryanatkn/eslint-config': '^0.5.5',
-		'@ryanatkn/fuz': '^0.129.3',
-		'@ryanatkn/gro': '^0.140.0',
+		'@ryanatkn/fuz': '^0.129.5',
+		'@ryanatkn/gro': '^0.140.4',
 		'@ryanatkn/moss': '^0.18.2',
 		'@sveltejs/adapter-static': '^3.0.5',
 		'@sveltejs/kit': '^2.7.0',
